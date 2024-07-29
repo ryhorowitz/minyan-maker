fd2bb277d2bfa4c26b22c36e03f405add2336a44
ryan horowitz <ryan.w.horowitz@gmail.com>
2024-07-29 17:08:14 -0400

    making bottom tab navigation

---
+++ navigation/BottomTabNavigation.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from '../screens/HomeScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import ConfirmScreen from '../screens/ConfirmScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabNavigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'ios-home';
+          } else if (route.name === 'Settings') {
+            iconName = 'ios-settings';
+          } else if (route.name === 'Confirm') {
+            iconName = 'ios-checkmark-circle';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Confirm" component={ConfirmScreen} />
+    </Tab.Navigator>
+  );
+}
